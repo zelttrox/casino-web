fc602191ce2827cb0057fdd4de53ab9967aa0a57
enzo <enzo.granana@ynov.com>
2025-05-13 17:52:42 +0200

    Add base API data

---
+++ api/init.js
@@ -0,0 +1,13 @@
+// Import scripts
+const data = require("./data")
+
+// Initialize API content
+var content = [
+    data.credits,
+    data.jackpot,
+    data.bets,
+    data.items
+]
+
+// Exports
+module.exports = content
