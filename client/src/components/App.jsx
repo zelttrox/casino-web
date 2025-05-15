c50530a46ca7cd31a6f3afb4865314a5c85bda64
enzo <enzo.granana@ynov.com>
2025-05-15 16:46:55 +0200

    Changes App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,37 +2,43 @@
 import './App.css'
 // Import librairies
 import { useState, useEffect } from 'react'
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: 'http://localhost:3000'
+});
 
 
 function App() {
   console.log("rendering")
 
   // Initialize hooks
-  const [data, setData] = useState({})
-  const [loading, setLoading] = useState(true)
+  // const [data, setData] = useState({})
+  // const [loading, setLoading] = useState(true)
 
-  useEffect( () => {
-    async function GetAPI() {
-      try {
-        const response = await fetch('/api')
-        setData(response.json())
-      }
-      catch (error) {
-        console.error("[React]", error)
-      }
-      finally {
-        setLoading(false)
-      }
-    }
-    GetAPI()
-  }, [])
+  // useEffect( () => {
+  //   async function GetAPI() {
+  //     try {
+  //       const response = await fetch('/api')
+  //       console.log(response)
+  //       setData(response.json())
+  //     }
+  //     catch (error) {
+  //       console.error("[React]", error)
+  //     }
+  //     finally {
+  //       setLoading(false)
+  //     }
+  //   }
+  //   GetAPI()
+  // }, [])
+
+  const data = api.get("/api")
 
   console.log(data)
 
   return (
     <>
-    {console.log("heifuh")}
     <p> Casino web </p>
     <p> Credits: {data.credits} </p>
     </>
--- readme.md
+++
@@ -1,2 +0,0 @@
-# casino-web
-Node.js + React casino web app simulator using an Azure database
